fb735a7300dbb3c80c171e4570315d1851ba798c
nxsyed <nxsyed.ahmed@gmail.com>
2018-07-10 14:42:12 -0700

    Completed frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import PubNubReact from 'pubnub-react';
+import { Button, Input, List } from 'antd';
+import './App.css';
 
 class App extends Component {
 
 constructor(props) {
   super(props);
   this.pubnub = new PubNubReact({ 
-    publishKey: 'pub-c-00eff6d9-ef6a-459c-9452-7e34aba11cba', 
-    subscribeKey: 'sub-c-6af8440e-4890-11e8-a3a7-d29c801c92ae' 
+    publishKey: 'Your Pub Key', 
+    subscribeKey: 'Your Sub Key' 
   });
   this.pubnub.init(this);
 }
 
 componentWillMount() {
   this.pubnub.subscribe({
-    channels: ['PWA']
+    channels: ['Alexa']
   });
-   
-  this.pubnub.getMessage('PWA', (msg) => {
-    this.notify(msg.message.text);
+
+  this.pubnub.getMessage('notify', (msg) => {
+    console.log(msg);
+    this.notify(msg);
   });
+}
+
+publish(userMessage) {
+  this.pubnub.publish(
+    {
+        message: {
+            message:userMessage
+        },
+        channel: 'Alexa',
+        storeInHistory: true
+    },
+    (status, response) => {
+        // handle status, response
+    }
+);
 }
 notify(message) {
 
@@ -45,10 +63,22 @@ notify(message) {
 }
 
   render() {
+    const messages = [
+        'Ok I\'ll be there in 5 mins',
+        'Call my number 415-xxx-6631',
+        'Alright, the door is open'
+      ];    
     return (
       <div className="App">
-        <button onClick={this.notify}>Send Notification
-        </button>
+        <List
+          header={<div>Choose a preselected response</div>}
+          bordered
+          dataSource={messages}
+          renderItem={item => (<List.Item>
+            <Button onClick={this.publish.bind(this, item)} type="primary">{item}</Button>
+            </List.Item>)
+            }
+        />
       </div>
     );
   }
